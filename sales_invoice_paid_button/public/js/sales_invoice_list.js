7fd20fa6ea2e35d7c8d8bbc9765fb323a9105bda
nehalpatil88 <nehalpatil1410@gmail.com>
2026-03-26 10:56:24 +0000

    working commits

--- sales_invoice_paid_button/public/js/sales_invoice_list.js
+++ sales_invoice_paid_button/public/js/sales_invoice_list.js
@@ -1,48 +1,50 @@
-console.log("Sales Invoice List JS Loaded");
+frappe.listview_settings['Sales Invoice'] = frappe.listview_settings['Sales Invoice'] || {};
 
-frappe.listview_settings['Sales Invoice'] = {
-    refresh(listview) {
-        console.log("Sales Invoice list refresh triggered");
+const old_refresh = frappe.listview_settings['Sales Invoice'].refresh;
 
-        if (listview.page.__paid_action_added) {
+frappe.listview_settings['Sales Invoice'].refresh = function (listview) {
+    if (old_refresh) {
+        old_refresh(listview);
+    }
+
+    if (listview.page.__paid_action_added) {
+        return;
+    }
+
+    listview.page.__paid_action_added = true;
+
+    listview.page.add_action_item(__('Mark as Paid'), async function () {
+        const selected = listview.get_checked_items();
+
+        if (!selected || !selected.length) {
+            frappe.msgprint(__('Please select at least one Sales Invoice.'));
             return;
         }
 
-        listview.page.__paid_action_added = true;
+        frappe.confirm(
+            __('Are you sure you want to mark the selected invoice(s) as Paid?'),
+            async function () {
+                frappe.dom.freeze(__('Processing...'));
 
-        listview.page.add_action_item(__('Mark as Paid'), async function () {
-            const selected = listview.get_checked_items();
-
-            if (!selected || !selected.length) {
-                frappe.msgprint(__('Please select at least one Sales Invoice.'));
-                return;
-            }
-
-            frappe.confirm(
-                __('Are you sure you want to mark the selected invoice(s) as Paid?'),
-                async function () {
-                    frappe.dom.freeze(__('Processing...'));
-
-                    try {
-                        for (const row of selected) {
-                            await frappe.call({
-                                method: 'sales_invoice_paid_button.api.mark_invoice_paid',
-                                args: {
-                                    docname: row.name
-                                }
-                            });
-                        }
-
-                        frappe.msgprint(__('Selected invoice(s) marked as Paid successfully.'));
-                        listview.refresh();
-                    } catch (e) {
-                        console.error(e);
-                        frappe.msgprint(__('Error while marking invoice(s) as Paid.'));
-                    } finally {
-                        frappe.dom.unfreeze();
+                try {
+                    for (const row of selected) {
+                        await frappe.call({
+                            method: 'sales_invoice_paid_button.api.mark_invoice_paid',
+                            args: {
+                                docname: row.name
+                            }
+                        });
                     }
+
+                    frappe.msgprint(__('Selected invoice(s) marked as Paid successfully.'));
+                    listview.refresh();
+                } catch (e) {
+                    console.error(e);
+                    frappe.msgprint(__('Error while marking invoice(s) as Paid.'));
+                } finally {
+                    frappe.dom.unfreeze();
                 }
-            );
-        });
-    }
+            }
+        );
+    });
 };
